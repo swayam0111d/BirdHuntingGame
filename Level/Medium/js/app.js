50aa097708f774a7aaaaaf74f66da403ee04883c
Swayam Raj <71974835+swayam0111d@users.noreply.github.com>
2024-08-14 16:53:50 +0530

    Add files via upload

---
+++ Level/Medium/js/app.js
@@ -0,0 +1,21 @@
+let count=0;
+let timecounter=45;
+
+setInterval(() => {
+    if(timecounter==0){
+       alert("Total Score :"+ count +"\n Reload the tab to play again ")
+    }else{
+        document.getElementById("currtimerid").innerHTML=timecounter--;
+    }
+}, 1000);
+
+
+function hideBird(clicked_id){
+    document.getElementById("currscoreid").innerHTML= ++count;
+    document.getElementById("shoot").play();
+   document.getElementById(clicked_id).style.display="none";
+   let x = Math.floor((Math.random()*10)+1);
+   setTimeout(() => {
+    document.getElementById(clicked_id).style.display="block";
+   },x*1000);
+}
